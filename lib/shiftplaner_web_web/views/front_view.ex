9dcb1b68730832abe12cbf3a3f04d35124d8401c
Friedrich Pfitzmann <friedrich@pfiss.com>
2017-09-12 11:14:58 +0200

    highlight empty shifts in red

--- lib/shiftplaner_web_web/views/front_view.ex
+++ lib/shiftplaner_web_web/views/front_view.ex
@@ -3,4 +3,12 @@ defmodule ShiftplanerWebWeb.FrontView do
 
   use ShiftplanerWebWeb, :view
 
+  def empty_worker_slot(list_of_persons, counter ) do
+    list_of_persons
+    |> Enum.at(counter - 1)
+    |> check_dispositioned()
+  end
+
+  defp check_dispositioned(person) when is_nil(person), do: "empty-shift-slot"
+  defp check_dispositioned(person), do: ""
 end
